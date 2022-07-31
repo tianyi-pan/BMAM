144569f314b626b1100a525cbd24404d4ef9d308
tianyi-pan <70828144+tianyi-pan@users.noreply.github.com>
2022-07-31 16:16:59 -0400

    fix bug for interactio in fix effects

--- R/generate_pred.R
+++ R/generate_pred.R
@@ -1,18 +1,18 @@
 #' @title generate predicted data
-#' 
+#'
 #' @param object Objects of Class 'brms'
 #' @param length number of observations in the generated data
 #'
 #' @return predicted data, used by \code{marginalcoef()}
 #'
 generate_pred <- function(object, length = 100){
-  mf <- model.frame(object) # data in object 
-  
+  mf <- model.frame(object) # data in object
+
   ## smooth term
-  smterm <- brmsterms(object$formula)$dpars$mu$sm # smooth term 
+  smterm <- brmsterms(object$formula)$dpars$mu$sm # smooth term
   stopifnot(!is.null(smterm)) # check
   smvariable <- lapply(smterm[[2]][-1], function(term.) term.[[2]]) # variable for smooth term
-  
+
   sm_pred <- lapply(smvariable, function(var){
     x <- mf[[var]]
     x <- x[which(x!=0)] # remove 0
@@ -22,18 +22,21 @@ generate_pred <- function(object, length = 100){
   })
   names(sm_pred) <- smvariable
   sm_pred <- do.call("cbind.data.frame", sm_pred)
-  
+
   ## fix effect term
   feterm <- brmsterms(object$formula)$dpars$mu$fe[[2]][-1]
-  
+
   if(feterm[[1]] == 1) feterm <- feterm[[-1]] # delete the intercept
-  
+
+  if(length(feterm[[2]]) > 1) feterm <- feterm[[1]] # delete interaction
+
   feterm <- lapply(feterm, function(var){
     if (length(var) > 1)  var = var[[3]]
+    if (var == "+") var = NULL
     var
   })
+  feterm[sapply(feterm, is.null)] <- NULL
 
-  
   if(!is.null(feterm)){
     fevariable <- feterm
     fe_pred <- lapply(fevariable, function(var){
@@ -47,7 +50,7 @@ generate_pred <- function(object, length = 100){
   }else{
     fe_pred <- NULL
   }
-  
+
   preddat <- lapply(smvariable, function(var){
     ## pred data for this variable
     sm_pred_design <- sm_pred; sm_pred_design[,] <- 0
@@ -56,8 +59,7 @@ generate_pred <- function(object, length = 100){
     preddat_var$varname <- as.character(var) # column to indicate the name of variable
     preddat_var
   })
-  
+
   preddat <- do.call("rbind",preddat)
-  
   return(preddat)
 }
