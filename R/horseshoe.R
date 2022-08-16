8a178eadc28fa4bea25abeb4f1ed24a3905e9e37
tianyi-pan <70828144+tianyi-pan@users.noreply.github.com>
2022-08-16 16:36:55 -0400

    horseshoe bf cmc

--- R/horseshoe.R
+++ R/horseshoe.R
@@ -18,8 +18,8 @@ brms.horseshoe <- function(shrinkage.term, nonshrinkage.term, ...){
   ## bf for brms
   if(length(shrinkage.term) == 3){
     formula <- bf(eval(expr(!!shrinkage.term[[2]] ~ a + b)) , nl = TRUE) +
-      lf(eval(expr(a ~ 0+!!shrinkage.term[[shrinkage.term.len]])), center = TRUE) +
-      lf(eval(expr(b ~ !!nonshrinkage.term[[nonshrinkage.term.len]])), cmc = FALSE)
+      lf(eval(expr(a ~ 0+!!shrinkage.term[[shrinkage.term.len]])), center = TRUE, cmc = FALSE) +
+      lf(eval(expr(b ~ !!nonshrinkage.term[[nonshrinkage.term.len]])))
   }else{
     formula <- bf(eval(expr(!!nonshrinkage.term[[2]] ~ a + b)) , nl = TRUE) +
       lf(eval(expr(a ~ 0+!!shrinkage.term[[shrinkage.term.len]])), center = TRUE) +
